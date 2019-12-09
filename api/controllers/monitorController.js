a1d306cb9201415f1d5e68d7a1a0a948024885a6
Chris Kerr <chriskerr@me.com>
2019-12-10 01:41:35 +1100

    split free -h too

--- api/controllers/monitorController.js
+++ api/controllers/monitorController.js
@@ -11,10 +11,14 @@ async function getPiStatus () {
 
     try {
         output.uptime = { stdout, stderr } = await exec('uptime');
-        output.uname = { stdout, stderr } = await exec('uname -nr'); // TO DO: change to "uname -nro" when on Rasp
-        output.free = { stdout, stderr } = await exec('free -h'); // TO DO: re-add when on Rasp
+        output.uname = { stdout, stderr } = await exec('uname -nro');
         
-        output.df = { stdout, stderr } = await exec('df -h'); // TO DO: re-add when on Rasp
+        output.free = { stdout, stderr } = await exec('free -h'); 
+        output.free.stdout = output.free.stdout.split('\n');
+        output.free.stdout = output.free.stdout.map(function(each) {return each.split(/\s+/)});
+        output.free.stdout[0].unshift('');
+        
+        output.df = { stdout, stderr } = await exec('df -h'); 
         output.df.stdout = output.df.stdout.split('\n');
         output.df.stdout = output.df.stdout.map(function(each) {return each.split(/\s+/)})
         output.df.stdout[0][5] += ' ' + output.df.stdout[0][6]; // wrong on Mac, should work on Pi
