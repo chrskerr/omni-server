96e51e9f55078bd6f07f5fc85b05815040ceb286
Chris Kerr <chriskerr@me.com>
2019-12-20 01:05:02 +1100

    turned CORS back on

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ const server = express();
 
 /// re-add when not using graphiql
 
-// const whitelist = [/localhost/, /chrskerr\.com/];
-// const corsOptionsDelegate = function (req, callback) {
-//     let corsOptions;
-//     whitelist.forEach( e => {
-//         if (req.header('Origin').match(e) !== -1) {
-//           corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//         } else {
-//         corsOptions = { origin: false } // disable CORS for this request
-//         }
-//     })
-//     callback(null, corsOptions) // callback expects two parameters: error and options
-// };
+const whitelist = [/localhost/, /chrskerr\.com/];
+const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    whitelist.forEach( e => {
+        if (req.header('Origin').match(e) !== -1) {
+          corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+        } else {
+        corsOptions = { origin: false } // disable CORS for this request
+        }
+    })
+    callback(null, corsOptions) // callback expects two parameters: error and options
+};
 
-server.use(cors());
-// server.use(cors(corsOptionsDelegate));
+// server.use(cors());
+server.use(cors(corsOptionsDelegate));
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 
